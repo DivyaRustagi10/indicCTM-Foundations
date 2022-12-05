3aad3aa769fd3f6c54c3fa380dd0316412506683
bostickt201 <bostickt201@gmail.com>
2022-12-05 18:04:17 -0500

    input validation added

--- deployment/app/pages/Tags_for_Nonnative_Speakers.py
+++ deployment/app/pages/Tags_for_Nonnative_Speakers.py
@@ -5,7 +5,12 @@ from sample_if5 import top_5
 from sample_if10 import top_10
 
 st.title("Looking for topic tags and nothing more? You've come to the right place.") # i.e., Nonnative Users
+
 #st.write("You have entered", st.session_state["my_input"])
+st.header("Note # 1:", anchor = None)
+st.text("Current functions include Top 10 and Top 5 best tags. These outputs are set to return for the sample output below:")
+st.text("Sample Case: ਗੁਜਰਾਤ ਦੇ 50 ਮਹਿਲਾ ਮੋਟਰਸਾਈਕਲ ਸਵਾਰਾਂ ਦੇ ਗਰੁੱਪ ‘ਬਾਈਕਿੰਗ ਕਵੀਨਜ਼’ ਨੇ ਅੱਜ ਇੱਥੇ ਪ੍ਰਧਾਨ ਮੰਤਰੀ, ਸ਼੍ਰੀ ਨਰੇਂਦਰ ਮੋਦੀ ਨਾਲ ਮੁਲਾਕਾਤ ਕੀਤੀ।ਗਰੁੱਪ ਦਾ ਕਹਿਣਾ ਹੈ ਕਿ ਉਨ੍ਹਾਂ ਨੇ 13 ਰਾਜਾਂ / ਕੇਂਦਰ ਸ਼ਾਸਤ ਪ੍ਰਦੇਸ਼ਾਂ ’ਚ 10,000 ਕਿਲੋਮੀਟਰ ਦਾ ਸਫਰ ਤੈਅ ਕਰਦਿਆਂ ਰਾਹ ਵਿੱਚ ਬੇਟੀ ਬਚਾਓ, ਬੇਟੀ ਪੜ੍ਹਾਓ ਅਤੇ ਸਵੱਛ ਭਾਰਤਵਰਗੇ ਕਈ ਸਮਾਜਿਕ ਮੁੱਦਿਆਂ ’ਤੇ ਲੋਕਾਂ ਨਾਲ ਚਰਚਾ ਕੀਤੀ।ਉਨ੍ਹਾਂ ਨੇ 15 ਅਗਸਤ 2017 ਨੂੰ ਲੱਦਾਖ ਦੇ ਖਰਦੁੰਗਲਾ ਵਿੱਖੇ ਤਿਰੰਗਾ ਲਹਿਰਾਇਆ।ਪ੍ਰਧਾਨ ਮੰਤਰੀ ਨੇ ਉਨ੍ਹਾਂ ਦੇ ਯਤਨਾਂ ਦੀ ਪ੍ਰਸ਼ੰਸਾ ਕੀਤੀ ਅਤੇ ਉਨ੍ਹਾਂ ਦੇ ਚੰਗੇ ਭਵਿੱਖ ਦੀ ਕਾਮਨਾ ਕੀਤੀ।"
+)
 
 # Start of 'Instructions' section #################
 st.header("Instructions", anchor=None)
@@ -30,64 +35,73 @@ disabled=False,
 label_visibility="visible")
 
 # Top 5 button
+
 if st.button('Get Top 5 Best Tags'):
-    st.spinner(text="Please wait ... Retrieving Top 5 best tags ...")
+    if text == '':
+        st.text("Please input text before hitting submit.")
 
-    # Fetch top 5 best tags
-    try:
+    else:
+        st.spinner(text="Please wait ... Retrieving Top 5 best tags ...")
 
-        # do something here with text
-        input = {"input_text": text}
+            # Fetch top 5 best tags
+        try:
 
-        preds = top_5()
+            # do something here with text
+            input = {"input_text": text}
+
+            preds = top_5()
         #res = requests.post(url = "https://a13c-34-143-172-49.ngrok.io/predict",
     #          data = json.dumps(input))
 
-        # Output top 5 best tags below
-        st.text("Top 5 from prediction_hi_model_25:")
-        st.text(preds[0])
+            # Output top 5 best tags below
+            st.text("Top 5 from prediction_hi_model_25:")
+            st.text(preds[0])
 
-        st.text("Top 5 from prediction_hi_model_50:")
-        st.text(preds[1])
+            st.text("Top 5 from prediction_hi_model_50:")
+            st.text(preds[1])
 
-        st.text("Top 5 from prediction_en_model_25:")
-        st.text(preds[2])
+            st.text("Top 5 from prediction_en_model_25:")
+            st.text(preds[2])
 
-        st.text("Top 5 from prediction_en_model_50:")
-        st.text(preds[3])
+            st.text("Top 5 from prediction_en_model_50:")
+            st.text(preds[3])
 
         #st.subheader(preds)
 
-    except ValueError:
-        st.text("Top 5 failed")
+        except ValueError:
+            st.text("Top 5 failed")
 
 
-# Top 10 button
+    # Top 10 button
 if st.button('Get Top 10 Best Tags'):
-    st.spinner(text="Please wait ... Retrieving Top 10 best tags ...")
+    if text == '':
+        st.text("Please input text before hitting submit.")
 
-    # Fetch top 10 best tags
-    try:
-        # do something here with text
-        input = { "input_text": text}
-#        res = requests.post(url = "https://a13c-34-143-172-49.ngrok.io/predict",
+    else:
+        st.spinner(text="Please wait ... Retrieving Top 10 best tags ...")
+
+        # Fetch top 10 best tags
+        try:
+            # do something here with text
+            input = { "input_text": text}
+#           res = requests.post(url = "https://a13c-34-143-172-49.ngrok.io/predict",
 #              data = json.dumps(input))
 
-        preds = top_10()
-        # Output top 10 best tags below
+            preds = top_10()
+            # Output top 10 best tags below
 
-        # Output top 10 best tags below
-        st.text("Top 10 from prediction_hi_model_25:")
-        st.text(preds[0])
+            # Output top 10 best tags below
+            st.text("Top 10 from prediction_hi_model_25:")
+            st.text(preds[0])
 
-        st.text("Top 10 from prediction_hi_model_50:")
-        st.text(preds[1])
+            st.text("Top 10 from prediction_hi_model_50:")
+            st.text(preds[1])
 
-        st.text("Top 10 from prediction_en_model_25:")
-        st.text(preds[2])
+            st.text("Top 10 from prediction_en_model_25:")
+            st.text(preds[2])
 
-        st.text("Top 10 from prediction_en_model_50:")
-        st.text(preds[3])
+            st.text("Top 10 from prediction_en_model_50:")
+            st.text(preds[3])
 
-    except ValueError:
-        st.text("Top 10 failed")
+        except ValueError:
+            st.text("Top 10 failed")
--- deployment/app/pages/Tags_for_Researchers_and_Multilingual_Speakers.py
+++ deployment/app/pages/Tags_for_Researchers_and_Multilingual_Speakers.py
@@ -10,12 +10,14 @@ from sample_if10 import top_10
 
 st.title("NLP researcher? Or just language savvy? Either way, add a little more flair to your topic tags.") # i.e., Multilingual/Research-oriented Users
 
-# Start of 'Instructions' section #################
-st.subheader("Note # 1: Current functions include Top 10 best tags and word cloud for English-based models. These outputs are set to return for the sample output below:")
+st.subheader("Note # 1:")
+st.text("Current functions include Top 10 best tags and word cloud for English-based models. These outputs are set to return for the sample output below:")
 st.text("Sample Case: ਗੁਜਰਾਤ ਦੇ 50 ਮਹਿਲਾ ਮੋਟਰਸਾਈਕਲ ਸਵਾਰਾਂ ਦੇ ਗਰੁੱਪ ‘ਬਾਈਕਿੰਗ ਕਵੀਨਜ਼’ ਨੇ ਅੱਜ ਇੱਥੇ ਪ੍ਰਧਾਨ ਮੰਤਰੀ, ਸ਼੍ਰੀ ਨਰੇਂਦਰ ਮੋਦੀ ਨਾਲ ਮੁਲਾਕਾਤ ਕੀਤੀ।ਗਰੁੱਪ ਦਾ ਕਹਿਣਾ ਹੈ ਕਿ ਉਨ੍ਹਾਂ ਨੇ 13 ਰਾਜਾਂ / ਕੇਂਦਰ ਸ਼ਾਸਤ ਪ੍ਰਦੇਸ਼ਾਂ ’ਚ 10,000 ਕਿਲੋਮੀਟਰ ਦਾ ਸਫਰ ਤੈਅ ਕਰਦਿਆਂ ਰਾਹ ਵਿੱਚ ਬੇਟੀ ਬਚਾਓ, ਬੇਟੀ ਪੜ੍ਹਾਓ ਅਤੇ ਸਵੱਛ ਭਾਰਤਵਰਗੇ ਕਈ ਸਮਾਜਿਕ ਮੁੱਦਿਆਂ ’ਤੇ ਲੋਕਾਂ ਨਾਲ ਚਰਚਾ ਕੀਤੀ।ਉਨ੍ਹਾਂ ਨੇ 15 ਅਗਸਤ 2017 ਨੂੰ ਲੱਦਾਖ ਦੇ ਖਰਦੁੰਗਲਾ ਵਿੱਖੇ ਤਿਰੰਗਾ ਲਹਿਰਾਇਆ।ਪ੍ਰਧਾਨ ਮੰਤਰੀ ਨੇ ਉਨ੍ਹਾਂ ਦੇ ਯਤਨਾਂ ਦੀ ਪ੍ਰਸ਼ੰਸਾ ਕੀਤੀ ਅਤੇ ਉਨ੍ਹਾਂ ਦੇ ਚੰਗੇ ਭਵਿੱਖ ਦੀ ਕਾਮਨਾ ਕੀਤੀ।"
 )
 
-st.subheader("Note # 2: Currently, only word cloud for English-based models and Top 10 tag functions exist in our 'Advanced Users' section. Future updates are set to include the below additional functions:")
+st.subheader("Note # 2:")
+
+st.text("Currently, only word cloud for English-based models and Top 10 tag functions exist in our 'Advanced Users' section. Future updates are set to include the below additional functions:")
 
 st.markdown("Topic Coherence score (NPMI)")
 st.markdown("Topic Similarity")
@@ -32,10 +34,11 @@ st.markdown('''
 </style>
 ''', unsafe_allow_html=True)
 
+# Start of 'Instructions' section #################
 st.header("Instructions", anchor=None)
+
 st.text("Copy and paste your text in the space below.")
 st.text("Feel free to use as many or as few characters as you'd like.")
-
 # End of 'Instructions' section #################
 
 # input user text
@@ -53,70 +56,88 @@ disabled=False,
 label_visibility="visible")
 
 if st.button('Submit'):
-    st.spinner(text="Please wait ... Retrieving tags ...")
+    if text == '':
+        st.text("Please input text before hitting submit.")
+
+    else:
+        st.spinner(text="Please wait ... Retrieving tags ...")
 
     # Fetch data and feed thru model
-    try:
+        try:
 
         # do something here with text
-        input = {"input_text": text}
+            input = {"input_text": text}
 #        res = requests.post(url = "https://a13c-34-143-172-49.ngrok.io/predict",
 #              data = json.dumps(input))
 
 
         # Start of 'Stats for Nerds' section
-        st.header("Stats for Nerds", anchor=None)
+            st.header("Stats for Nerds", anchor=None)
 
         # Output Word Cloud visualization below
 
         # get sample text
-        preds = top_10()
+            preds = top_10()
 
         # Output top 10 best tags
-        st.text("Top 10 from prediction_hi_model_25:")
-        st.text(preds[0])
+            st.text("Top 10 from prediction_hi_model_25:")
+            st.text(preds[0])
 
         # Create and generate a word cloud image:
-        hi_25 = preds[0]
-        hi_25_str = ','.join(str(item) for item in hi_25)
+            hi_25 = preds[0]
+            hi_25_str = ','.join(str(item) for item in hi_25)
+
+        #wordcloud = WordCloud().generate(hi_25_str)
+
+        #fig, ax = plt.subplots(figsize = (12, 8))
+        #ax.imshow(wordcloud)
+        #plt.axis("off")
+        #st.pyplot(fig)
 
         # Output top 10 best tags
-        hi_50 = preds[1]
-        hi_50_str = ','.join(str(item) for item in hi_50)
+            hi_50 = preds[1]
+            hi_50_str = ','.join(str(item) for item in hi_50)
 
-        st.text("Top 10 from prediction_hi_model_50:")
-        st.text(preds[1])
+            st.text("Top 10 from prediction_hi_model_50:")
+            st.text(preds[1])
 
-        # Output top 10 best tags
-        st.text("Top 10 from prediction_en_model_25:")
-        st.text(preds[2])
+        #wordcloud = WordCloud().generate(hi_50_str)
 
-        en_25 = preds[2]
-        en_25_str = ','.join(str(item) for item in en_25)
-
-        cloud = WordCloud().generate(en_25_str)
-
-        fig, ax = plt.subplots(figsize = (12, 8))
-        ax.imshow(cloud)
-        plt.axis("off")
-        st.pyplot(fig)
+        #fig, ax = plt.subplots(figsize = (12, 8))
+        #ax.imshow(wordcloud)
+        #plt.axis("off")
+        #st.pyplot(fig)
 
         # Output top 10 best tags
-        en_50 = preds[3]
-        en_50_str = ','.join(str(item) for item in en_50)
+            st.text("Top 10 from prediction_en_model_25:")
+            st.text(preds[2])
 
-        st.text("Top 10 from prediction_en_model_50:")
-        st.text(preds[3])
+            en_25 = preds[2]
+            en_25_str = ','.join(str(item) for item in en_25)
 
-        cloud = WordCloud().generate(en_50_str)
+            cloud = WordCloud().generate(en_25_str)
 
-        fig, ax = plt.subplots(figsize = (12, 8))
-        ax.imshow(cloud)
-        plt.axis("off")
-        st.pyplot(fig)
+            fig, ax = plt.subplots(figsize = (12, 8))
+            ax.imshow(cloud)
+            plt.axis("off")
+            st.pyplot(fig)
+
+        # Output top 10 best tags
+            en_50 = preds[3]
+            en_50_str = ','.join(str(item) for item in en_50)
+
+            st.text("Top 10 from prediction_en_model_50:")
+            st.text(preds[3])
+
+            cloud = WordCloud().generate(en_50_str)
+
+            fig, ax = plt.subplots(figsize = (12, 8))
+            ax.imshow(cloud)
+            plt.axis("off")
+            st.pyplot(fig)
 
         # Output request results below
         #st.subheader(input)
 
-    except ValueError:
-        st.text("Submit failed")
+        except ValueError:
+            st.text("Submit failed")
